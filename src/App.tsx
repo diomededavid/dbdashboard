f88a267ea9349dc2a67dba750156327d2f8936b3
Angel <angel@Angels-MacBook-Pro.local>
2022-03-05 12:12:19 -0700

    Add Logo to the sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-=======
-import React from 'react';
-import './App.css';
->>>>>>> 9c71a097d74c03cdadbcdcb6cf924c17ca81f636
 import Card from "./components/Card";
 import NavBar from "./components/NavBar";
 import Sidebar from "./components/Sidebar";
@@ -13,16 +8,10 @@ import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <NavBar />
+      {/* <NavBar /> */}
       <Sidebar />
       <header className="App-header">
-        <Card title="Title" description="description" />
-        <Tag statusText="High" />
-=======
-        <NavBar/>
-        <header className="App-header">
->>>>>>> 9c71a097d74c03cdadbcdcb6cf924c17ca81f636
+        <Card>Test</Card>
       </header>
     </div>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import logo from "../images/db-asterisk-logo-white.svg";
 
 const Sidebar = () => {
   return (
     <div className="flex min-h-screen">
-      <div className="w-64 to-blue-800 border-r border-blue-800">Sidebar </div>
-      <div className="py-4 px-6"></div>
+      <div className="w-64 bg-blue-800 border-r border-blue-800">
+        <div className="py-4 px-6">
+          <img className="w-full" width="100" src={logo} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
