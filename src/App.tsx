f1b7efc9254f6a2ff2874016e3553909cf51cd91
diomededavid <david@d2creativedesign.com>
2022-03-05 11:56:15 -0700

    pull fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,20 @@
-<<<<<<< HEAD
-import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
-=======
 import React from 'react';
 import './App.css';
->>>>>>> 9c71a097d74c03cdadbcdcb6cf924c17ca81f636
 import Card from "./components/Card";
+import Tag from "./components/Tag";
 import NavBar from "./components/NavBar";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <NavBar />
       <Sidebar />
       <header className="App-header">
-        <Card title="Title" description="description" />
+        <Card >
         <Tag statusText="High" />
-=======
-        <NavBar/>
-        <header className="App-header">
->>>>>>> 9c71a097d74c03cdadbcdcb6cf924c17ca81f636
+        </Card>
       </header>
     </div>
   );
