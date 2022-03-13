8b0293e4ced04618159fb6696fa5a4753b6fe1db
diomededavid <david@d2creativedesign.com>
2022-03-12 17:03:21 -0700

    remove imort react from react, no longer needed

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,11 +1,11 @@
-import React from "react";
 const Avatar = () => {
   return (
     <div className="avatar">
       <div className="w-12 rounded-full">
-        <img src="https://api.lorem.space/image/face?hash=92310" />
+        <img src="https://api.lorem.space/image/face?hash=92310" alt="avatar"/>
       </div>
     </div>
   );
 };
+
 export default Avatar;
